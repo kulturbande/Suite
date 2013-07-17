3fde3f1147d7f110aeb3a0b748d483de17b12e47
Sascha Karnatz <sascha@kulturbande.de>
2013-07-17 08:28:34 +0200

    Include pure.css

--- app.js
+++ app.js
@@ -22,6 +22,7 @@ app.use(express.bodyParser());
 app.use(express.methodOverride());
 app.use(app.router);
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(require('connect-assets')());
 
 // development only
 if ('development' == app.get('env')) {
